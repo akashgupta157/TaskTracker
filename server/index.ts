b544bdd525cf7d2cc3db0a2e40bb8f54164e9606
Akash Gupta <akashgupta989322@gmail.com>
2023-10-09 16:09:17 +0530

    update

--- server/index.ts
+++ server/index.ts
@@ -21,7 +21,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://tasktrackerpro.vercel.app",
     methods: "GET,POST,PUT,PATCH,DELETE",
     credentials: true,
   })
