e279dc6e48c735c491dfdae8ae1aea5253c3ce80
Akash Gupta <akashgupta989322@gmail.com>
2023-10-10 22:41:54 +0530

    update

--- client/src/components/url.ts
+++ client/src/components/url.ts
@@ -1,2 +1,2 @@
-// export const url = 'https://tasktracker-7obs.onrender.com'
-export const url = 'http://localhost:3000'
+export const url = 'https://tasktracker-7obs.onrender.com'
+// export const url = 'http://localhost:3000'
