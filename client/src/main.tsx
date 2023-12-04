043afaaabd447745aeb1a64eb3dfff4b47ba883c
Akash Gupta <akashgupta989322@gmail.com>
2023-12-04 15:18:53 +0530

    update

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,7 +8,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <BrowserRouter>
-      <GoogleOAuthProvider clientId={'817866145023-u2r3ksfo2io1v7rjsan0gnp30u3uvrss.apps.googleusercontent.com'} >
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_ClientId} >
         <App />
       </GoogleOAuthProvider>
     </BrowserRouter>
