1e72335935436abfff6140aa77f45baf225e79c3
Akash Gupta <akashgupta989322@gmail.com>
2024-10-17 20:01:37 +0530

    update

--- client/src/components/url.ts
+++ client/src/components/url.ts
@@ -1,2 +1,2 @@
-// export const url = 'https://muddy-khakis-jay.cyclic.app'
-export const url = 'http://localhost:3000'
+export const url = 'https://tasktracker-b0qh.onrender.com'
+// export const url = 'http://localhost:3000'
