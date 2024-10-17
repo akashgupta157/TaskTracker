0c84cbca8a280df334d5ef6de11a36a4514977b0
Akash Gupta <akashgupta989322@gmail.com>
2024-10-17 14:34:36 +0530

    update

--- client/src/components/url.ts
+++ client/src/components/url.ts
@@ -1,2 +1,2 @@
-export const url = 'https://muddy-khakis-jay.cyclic.app'
-// export const url = 'http://localhost:3000'
+// export const url = 'https://muddy-khakis-jay.cyclic.app'
+export const url = 'http://localhost:3000'
